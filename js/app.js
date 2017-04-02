d8c6c3c93b9e0f5dbb29c82b1434234db09288bf
MarMinsk <mwheatley@hotmail.com>
2017-04-02 22:00:02 +0100

    updated game logic pseudocode

--- js/app.js
+++ js/app.js
@@ -1,30 +1,53 @@
-// console.log('hello');
-// Tube map memory game:
-//
-// Player clicks start to start the game
-// Player is presented with one of the 11 London Underground lines (randomly generated)
+$(() => {
+  // console.log('Ready'); is this all that need to be done to set up jQuery???
+
+  $('li').on('click', function() {
+    console.log($(this).attr('class'));
+  });
+});
+
+// Tube map memory game - game play:
+// Player is presented with a one of the 11 London Underground lines (randomly generated)
 // Player is also presented with three possible options for number of stations (displayed as buttons)
 // One of the options is correct, the other two are incorrect
 // Player clicks on the button they think represents the correct number
-// Player then clicks on the Submit button to submit their selection
+// Player then clicks on the 'Submit' button to submit their selection
 // Player is then presented with another randomly generated line and repeats the process
 // Process repeats until selections for all 11 lines have been submitted
 // Computer then displays how many selections the player got correct (out of 11)
 // Game is timed and timer starts as soon as the first line is displayed
 
+// Pseudocode (for game logic)
+// player will click on 'Start' button to start the game (and timer? Where will 'Start' button sit on page?)
+// game will require a for loop to iterate through the lines.length???
+// function that will randomly select a 'line' (from the 11 available key/value pairs)
+// function that will display this selection in the 'line name' div
+// function that will take the correct 'station number' value from the relevant key/value pair
+// function that will put this number into one of the three available 'li' elements (selected at ramdom)
+// function that will also generate two incorrect 'station number' options (using Math.random?)
+// function that will put these two numbers into the other two available 'li' elements
+// on.click function to allow player ot make their selection by clicking on 'li' element
+// (also need to include a function that allows player to change their choice)
+// on.click(??) on 'Submit' (button??) to allow player to submit their choice
+// game ends if player makes incorrect selection
+// function to hold player's choices until end of game (is this held in an array?)
+// if player makes correct selection, game iterates through to next randomly selected line
+// need to include a timer (To start as soon as player clicks on 'start'? Where will Timer sit on the page?)
 
+// key/value pair of lines & station numbers:
 const lines = {
-  'Bakerloo': 25;
-  'Central': 49;
-  'Circle': 36;
-  'District': 60;
-  'Hammersmith & City': 29;
-  'Jubilee': 27;
-  'Metropolitan': 34;
-  'Northern': 50;
-  'Picadilly': 53;
-  'Victoria': 16;
-  'Waterloo & City': 2;
+  'Bakerloo': 25,
+  'Central': 49,
+  'Circle': 36,
+  'District': 60,
+  'Hammersmith & City': 29,
+  'Jubilee': 27,
+  'Metropolitan': 34,
+  'Northern': 50,
+  'Picadilly': 53,
+  'Victoria': 16,
+  'Waterloo & City': 2
+  // is it correct that lines end w , rather than ;???
 };
 
 
